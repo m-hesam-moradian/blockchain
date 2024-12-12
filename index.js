697ed7ceb01f8c205ecb3cc9d57d4386d7802f7d
m.hesam moradian <m.hesam.moradian@gmail.com>
2024-12-12 18:09:36 +0330

    my first blockchain

--- index.js
+++ index.js
@@ -6,10 +6,23 @@ class Block {
   }
 }
 class Blockchain {
-  constructor(chain) {
-    this.chain = [];
+  constructor() {
+    const genesisBLock = new Block("gen-data", "gen-hash", "gen-lastHash");
+    this.chain = [genesisBLock];
+  }
+  addBlock(data) {
+    const lastHash = this.chain[this.chain.length - 1].hash;
+    const hash = hashFunc(data + lastHash);
+    const block = new Block(data, hash, lastHash);
+    this.chain.push(block);
   }
 }
 const hashFunc = (input) => {
   return "*" + input + "*";
 };
+
+const fooBlockchain = new Blockchain();
+fooBlockchain.addBlock("one");
+fooBlockchain.addBlock("two");
+fooBlockchain.addBlock("three");
+console.log(fooBlockchain);
